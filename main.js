ca4abfb3186676694ae3a246c3e2a9c16e9b156b
Vikitohyah <victoriaedorh@gmail.com>
2024-09-02 04:27:49 +0100

    JavaScript Age Calc

---
+++ main.js
@@ -0,0 +1,8 @@
+function lifeInWeeks(age) {
+    const yearsRemaining = 90 - age;
+    const daysRemaining = yearsRemaining * 365;
+    const weeksRemaining = yearsRemaining * 52;
+    const monthsRemaining = yearsRemaining * 12;
+
+    console.log(`You have ${daysRemaining} days, ${weeksRemaining} weeks, and ${monthsRemaining} months left.`);
+}
